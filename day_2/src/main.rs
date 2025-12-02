719afd61eb8581864519657ede913571f527de66
satetheus <personal@cwmccullough.com>
2025-12-02 00:14:25 -0700

    planning for day 2

---
+++ day_2/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // get maximum length of repeatable pattern
+    // get limit from maximum in range
+    // increment through all the possible repeats to validate
+    // store the repeats as they come
+    // sum them at the end (could also do rolling sum)
+    todo!();
+}
