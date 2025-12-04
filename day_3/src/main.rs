247d1ede0df653b92f6f06db46fcfc33cf0aea10
satetheus <personal@cwmccullough.com>
2025-12-03 18:12:43 -0700

    working day 3 part 1 logic

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -6,8 +6,27 @@ fn process_file(file: &Path) {
 }
 
 
-fn get_joltage(battery: i64) -> i64 {
-    todo!();
+fn get_joltage(battery: String) -> i32 {
+    let mut max_digit = '0';
+    let mut max_digit_index = 0;
+    let digits = battery.chars().collect::<Vec<_>>();
+
+    for (i, digit) in digits.iter().enumerate().take(digits.len() - 1) {
+        if *digit > max_digit {
+            max_digit = *digit;
+            max_digit_index = i;
+        }
+    }
+
+    let mut next_max_digit = '0';
+    for digit in digits.iter().skip(max_digit_index+1) {
+        if *digit > next_max_digit {
+            next_max_digit = *digit;
+        }
+    }
+
+    (max_digit.to_string()+&next_max_digit.to_string())
+        .parse::<i32>().expect("not a number")
 }
 
 
@@ -22,9 +41,9 @@ mod tests {
 
     #[test]
     fn test_get_joltage() {
-        assert_eq!(get_joltage(987654321111111), 98);
-        assert_eq!(get_joltage(811111111111119), 89);
-        assert_eq!(get_joltage(234234234234278), 78);
-        assert_eq!(get_joltage(818181911112111), 92);
+        assert_eq!(get_joltage("987654321111111".to_string()), 98);
+        assert_eq!(get_joltage("811111111111119".to_string()), 89);
+        assert_eq!(get_joltage("234234234234278".to_string()), 78);
+        assert_eq!(get_joltage("818181911112111".to_string()), 92);
     }
 }
