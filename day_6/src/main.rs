9f54bf2b0592e04d870ce824e4a48ecad5bc6f44
satetheus <personal@cwmccullough.com>
2025-12-07 02:55:50 -0700

    almost working day 6 code. just needs to process the numbers

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -5,21 +5,18 @@ use std::path::Path;
 
 fn main() {
     let input = process_file("day_6_input.txt");
-    let pivot = transpose(input);
-    let part_1_output = do_operations(pivot);
+    let part_1_output = do_operations(input);
     println!("{:?}", part_1_output);
 }
 
 
-fn process_file(filename: impl AsRef<Path>) -> Vec<Vec<String>> {
+fn process_file(filename: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(filename).expect("file not found");
     let contents = BufReader::new(file);
 
-    let lines: Vec<String> = contents.lines()
+    contents.lines()
         .map(|n| n.expect("couldn't parse line"))
-        .collect();
-
-    lines.iter().map(|n| n.split_whitespace().map(String::from).collect()).collect()
+        .collect::<Vec<String>>()
 }
 
 
@@ -34,10 +31,12 @@ fn transpose<T>(input: Vec<Vec<T>>) -> Vec<Vec<T>> {
 }
 
 
-fn do_operations(input: Vec<Vec<String>>) -> i64 {
+fn do_operations(input: Vec<String>) -> i64 {
+    let parsed_input = input.iter().map(|n| n.split_whitespace().map(String::from).collect()).collect();
+    let pivot = transpose(parsed_input);
     let mut total = 0;
 
-    for mut set in input {
+    for mut set in pivot {
         let operator = set.pop().expect("no operator found");
         let numbers: Vec<i64> = set.iter().map(|n| n.parse().expect("not a number")).collect();
         if operator == "+" {
@@ -55,6 +54,27 @@ fn do_operations(input: Vec<Vec<String>>) -> i64 {
 }
 
 
+fn do_wonky_operations(input: Vec<String>) -> i64 {
+    let parsed_input: Vec<Vec<char>> = input.iter().map(|n| n.chars().collect()).collect();
+    let mut total = 0;
+    let pivot = transpose(parsed_input);
+    let mut operators: Vec<char> = vec![];
+    let mut numbers: Vec<i64> = vec![];
+    for mut set in pivot {
+        operators.push(set.pop().expect("no operator found"));
+        numbers.push(set.iter().filter(|n| **n != ' ').collect::<String>().parse().unwrap_or(0));
+    }
+    dbg!(&numbers,&operators);
+
+    let mut action = ' ';
+    for (i, operator) in operators.iter().enumerate() {
+        if *operator != ' ' { action = *operator; }
+    }
+
+    total
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -62,8 +82,12 @@ mod tests {
     #[test]
     fn test_do_operations() {
         let input: Vec<String> = vec!["123 328 51 64".to_string(),"45 64 387 23".to_string(),"6 98 215 314".to_string(),"* + * +".to_string()];
-        let processed_input = input.iter().map(|n| n.split_whitespace().map(String::from).collect()).collect();
-        let pivoted_input = transpose(processed_input);
-        assert_eq!(do_operations(pivoted_input), 4277556);
+        assert_eq!(do_operations(input), 4277556);
+    }
+
+    #[test]
+    fn test_do_wonky_operations() {
+        let input: Vec<String> = vec!["123 328  51 64 ".to_string()," 45 64  387 23 ".to_string(),"  6 98  215 314".to_string(),"*   +   *   +  ".to_string()];
+        assert_eq!(do_wonky_operations(input), 3263827);
     }
 }
