8ae48d0e4851ee666b9dd9aaff28f91a3957d552
satetheus <personal@cwmccullough.com>
2025-12-15 00:08:33 -0700

    untangled day 2 process_file function & ran formatter on the code

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,40 +1,32 @@
 use std::collections::HashSet;
-use std::path::Path;
 use std::fs;
+use std::path::Path;
 use utils::split_str;
-
 
 fn main() {
     let ranges = process_file("day_2_input.txt");
     let mut total_p1: i64 = 0;
     let mut total_p2: i64 = 0;
     for range in &ranges {
-        total_p1 += find_invalid_ids(range).0.iter().sum::<i64>();
-        total_p2 += find_invalid_ids(range).1.iter().sum::<i64>();
+        let invalids = find_invalid_ids(range);
+        total_p1 += invalids.0.iter().sum::<i64>();
+        total_p2 += invalids.1.iter().sum::<i64>();
     }
     println!("Part 1: {:?}", total_p1);
     println!("Part 2: {:?}", total_p2);
 }
 
-
 fn process_file(filename: impl AsRef<Path>) -> Vec<Vec<i64>> {
     let file = fs::read_to_string(filename).expect("file not found");
     let contents = file.strip_suffix("\n").expect("Couldn't strip newlines");
 
-    // Not proud of this nested closure. Something is probably wrong here
-    // but the output is correct.
-    let lines: Vec<String> = split_str!(contents,',');
-    let split_lines: Vec<Vec<_>> = lines.iter().map(|l|
-        l.split('-').collect::<Vec<_>>().into_iter().map(|s|
-            s.parse::<i64>().expect("error converting to number")
-        ).collect()
-    ).collect();
+    let lines: Vec<String> = split_str!(contents, ',');
+    let split_lines: Vec<Vec<i64>> = lines.iter().map(|l| split_str!(l, '-')).collect();
 
     split_lines
 }
 
-
-fn find_invalid_ids(range: &[i64]) -> (Vec<i64>,HashSet<i64>) {
+fn find_invalid_ids(range: &[i64]) -> (Vec<i64>, HashSet<i64>) {
     let mut invalids_p1: Vec<i64> = vec![];
     let mut invalids_p2: HashSet<i64> = HashSet::new();
 
@@ -43,24 +35,21 @@ fn find_invalid_ids(range: &[i64]) -> (Vec<i64>,HashSet<i64>) {
         let chunks = get_multiples(id.len());
         for chunk_size in chunks {
             if check_id_by_chunk(&id, chunk_size) {
-                if id.len() % 2 == 0 && chunk_size == id.len()/2 {
+                if id.len() % 2 == 0 && chunk_size == id.len() / 2 {
                     invalids_p1.push(i);
                 }
 
                 invalids_p2.insert(i);
             }
         }
-
     }
 
     (invalids_p1, invalids_p2)
 }
 
-
 fn get_multiples(num: usize) -> Vec<usize> {
-    (1..=num/2).filter(|i| num % i == 0).collect()
+    (1..=num / 2).filter(|i| num % i == 0).collect()
 }
-
 
 fn check_id_by_chunk(id: &str, chunk_size: usize) -> bool {
     let mut test = id.as_bytes().chunks(chunk_size);
@@ -69,36 +58,62 @@ fn check_id_by_chunk(id: &str, chunk_size: usize) -> bool {
     test.all(|n| n == chunk_1)
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_check_id_by_chunk() {
-        assert!(check_id_by_chunk("11",1));
-        assert!(!check_id_by_chunk("12",1));
-        assert!(!check_id_by_chunk("1011",2));
-        assert!(check_id_by_chunk("1010",2));
-        assert!(!check_id_by_chunk("1010",1));
+        assert!(check_id_by_chunk("11", 1));
+        assert!(!check_id_by_chunk("12", 1));
+        assert!(!check_id_by_chunk("1011", 2));
+        assert!(check_id_by_chunk("1010", 2));
+        assert!(!check_id_by_chunk("1010", 1));
     }
 
     #[test]
     fn test_find_invalid_ids() {
-        assert_eq!(find_invalid_ids(&[11,22]), (vec![11,22],HashSet::from([11,22])));
-        assert_eq!(find_invalid_ids(&[95,115]), (vec![99],HashSet::from([99,111])));
-        assert_eq!(find_invalid_ids(&[998,1012]), (vec![1010],HashSet::from([999,1010])));
-        assert_eq!(find_invalid_ids(&[1188511880,1188511890]), (vec![1188511885],HashSet::from([1188511885])));
-        assert_eq!(find_invalid_ids(&[222220,222224]), (vec![222222],HashSet::from([222222])));
-        assert_eq!(find_invalid_ids(&[1698522,1698528]), (vec![],HashSet::from([])));
-        assert_eq!(find_invalid_ids(&[446443,446449]), (vec![446446],HashSet::from([446446])));
-        assert_eq!(find_invalid_ids(&[38593856,38593862]), (vec![38593859],HashSet::from([38593859])));
-        assert_eq!(find_invalid_ids(&[2121212118,2121212124]), (vec![],HashSet::from([2121212121])));
+        assert_eq!(
+            find_invalid_ids(&[11, 22]),
+            (vec![11, 22], HashSet::from([11, 22]))
+        );
+        assert_eq!(
+            find_invalid_ids(&[95, 115]),
+            (vec![99], HashSet::from([99, 111]))
+        );
+        assert_eq!(
+            find_invalid_ids(&[998, 1012]),
+            (vec![1010], HashSet::from([999, 1010]))
+        );
+        assert_eq!(
+            find_invalid_ids(&[1188511880, 1188511890]),
+            (vec![1188511885], HashSet::from([1188511885]))
+        );
+        assert_eq!(
+            find_invalid_ids(&[222220, 222224]),
+            (vec![222222], HashSet::from([222222]))
+        );
+        assert_eq!(
+            find_invalid_ids(&[1698522, 1698528]),
+            (vec![], HashSet::from([]))
+        );
+        assert_eq!(
+            find_invalid_ids(&[446443, 446449]),
+            (vec![446446], HashSet::from([446446]))
+        );
+        assert_eq!(
+            find_invalid_ids(&[38593856, 38593862]),
+            (vec![38593859], HashSet::from([38593859]))
+        );
+        assert_eq!(
+            find_invalid_ids(&[2121212118, 2121212124]),
+            (vec![], HashSet::from([2121212121]))
+        );
     }
 
     #[test]
     fn test_get_multiples() {
-        assert_eq!(get_multiples(6), vec![1,2,3]);
+        assert_eq!(get_multiples(6), vec![1, 2, 3]);
         assert_eq!(get_multiples(5), vec![1]);
         assert_eq!(get_multiples(2), vec![1]);
     }
